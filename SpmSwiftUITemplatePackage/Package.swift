6618a0e685f06fb54b919689cb6a056ed873db08
mui-z <93278577+mui-z@users.noreply.github.com>
2024-02-21 00:37:22 +0900

    chore: update version

--- SpmSwiftUITemplatePackage/Package.swift
+++ SpmSwiftUITemplatePackage/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "SpmSwiftUITemplatePackage",
-  platforms: [.iOS(.v16)],
+  platforms: [.iOS(.v17)],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
