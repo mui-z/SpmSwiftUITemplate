3f1edaa71e6bb2f9274137bc97ac313e81fb94e8
mui-z <93278577+mui-z@users.noreply.github.com>
2025-02-01 12:47:34 +0900

    chore: update swiftformat lang version

--- SpmSwiftUITemplatePackage/Sources/SpmSwiftUITemplatePackage/ContentView.swift
+++ SpmSwiftUITemplatePackage/Sources/SpmSwiftUITemplatePackage/ContentView.swift
@@ -1,6 +1,5 @@
-import SwiftUI
 import ComposableArchitecture
-
+import SwiftUI
 
 @Reducer
 public struct Content {
@@ -8,34 +7,33 @@ public struct Content {
   public struct State {
     var count = 0
   }
-  
+
   public enum Action {
     case increment
     case decrement
   }
-  
+
   public var body: some Reducer<State, Action> {
     Reduce { state, action in
       switch action {
-        case .increment:
-          state.count += 1
-          return .none
-        case .decrement:
-          state.count -= 1
-          return .none
+      case .increment:
+        state.count += 1
+        return .none
+      case .decrement:
+        state.count -= 1
+        return .none
       }
     }
   }
 }
 
-
 public struct ContentView: View {
   public init() {
     store = Store(initialState: .init(), reducer: { Content() })
   }
-  
+
   public let store: StoreOf<Content>
-  
+
   public var body: some View {
     VStack {
       Text(store.count.description)
