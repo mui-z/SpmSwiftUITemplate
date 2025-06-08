92219d0411837aa9d623e70d9b4f334be61cb52e
mui-z <93278577+mui-z@users.noreply.github.com>
2025-06-08 12:27:50 +0900

    chore: remove tca

--- SpmSwiftUITemplatePackage/Package.swift
+++ SpmSwiftUITemplatePackage/Package.swift
@@ -8,18 +8,10 @@ let package = Package(
   platforms: [.iOS(.v18)],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "SpmSwiftUITemplatePackage",
-      targets: ["SpmSwiftUITemplatePackage"]
-    ),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(
-      url: "https://github.com/pointfreeco/swift-composable-architecture",
-      from: "1.0.0"
-    ),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,10 +19,6 @@ let package = Package(
     .target(
       name: "SpmSwiftUITemplatePackage",
       dependencies: [
-        .product(
-          name: "ComposableArchitecture",
-          package: "swift-composable-architecture"
-        ),
       ]
     ),
     .testTarget(
--- SpmSwiftUITemplatePackage/Sources/SpmSwiftUITemplatePackage/ContentView.swift
+++ SpmSwiftUITemplatePackage/Sources/SpmSwiftUITemplatePackage/ContentView.swift
@@ -1,50 +1,11 @@
-import ComposableArchitecture
 import SwiftUI
 
-@Reducer
-public struct Content {
-  @ObservableState
-  public struct State {
-    var count = 0
-  }
-
-  public enum Action {
-    case increment
-    case decrement
-  }
-
-  public var body: some Reducer<State, Action> {
-    Reduce { state, action in
-      switch action {
-      case .increment:
-        state.count += 1
-        return .none
-      case .decrement:
-        state.count -= 1
-        return .none
-      }
-    }
-  }
-}
 
 public struct ContentView: View {
   public init() {
-    store = Store(initialState: .init(), reducer: { Content() })
   }
 
-  public let store: StoreOf<Content>
-
   public var body: some View {
-    VStack {
-      Text(store.count.description)
-      HStack {
-        Button("-") {
-          store.send(.decrement)
-        }
-        Button("+") {
-          store.send(.increment)
-        }
-      }
-    }
+      Text("Hello World!")
   }
 }
